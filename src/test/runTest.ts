b07f259a4fca3013a11b65a7757c7972e919c08f
Taehwan Kwag <thkwag@gmail.com>
2025-01-02 22:00:17 +0900

    Enhance test execution by suppressing unnecessary messages and adding additional launch arguments

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,6 +3,13 @@ import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath, runTest
 
 async function main() {
     try {
+        // Suppress unnecessary messages and warnings
+        process.env.ELECTRON_DISABLE_GPU = '1';
+        process.env.ELECTRON_NO_ATTACH_CONSOLE = '1';
+        process.env.ELECTRON_ENABLE_LOGGING = '0';
+        process.env.NODE_NO_WARNINGS = '1';
+        process.env.VSCODE_SKIP_BUILTIN_EXTENSION_LOAD = '1';
+
         const vscodeExecutablePath = await downloadAndUnzipVSCode();
         const [, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
 
@@ -13,7 +20,18 @@ async function main() {
             vscodeExecutablePath,
             extensionDevelopmentPath,
             extensionTestsPath,
-            launchArgs: [...args, '--disable-extensions']
+            launchArgs: [
+                ...args,
+                '--disable-extensions',
+                '--disable-gpu',
+                '--disable-updates',
+                '--skip-welcome',
+                '--skip-release-notes',
+                '--disable-workspace-trust',
+                '--disable-telemetry',
+                '--no-sandbox',
+                '--disable-crash-reporter'
+            ]
         });
     } catch (err) {
         console.error('Failed to run tests:', err);
